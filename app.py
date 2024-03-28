6c4605eee29dca41038d029730d80a23bb19fe03
arthurcornelio <arthur.cornelio@gmail.com>
2024-03-28 16:25:06 +0100

    modifications for working bar try2

--- app.py
+++ app.py
@@ -608,8 +608,8 @@ elif st.session_state.current_question == 8:
             for percent_complete in range(100):
                 time.sleep(0.01)
                 my_bar.progress(percent_complete + 1, text=progress_text)
-            time.sleep(1)
-            my_bar.empty()
+            #time.sleep(1)
+            #my_bar.empty()
 
             #### END ARTHUR's MODIFICATIONS####
 
